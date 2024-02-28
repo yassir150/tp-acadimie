6557335e9bc4dd2f9cfe30e5c7b6c295df580a4c
yassir150 <yassirbjarho@gmail.com>
2024-02-28 12:07:28 +0100

    version 2.5
    les etudiants :
    >akram blial
    >amine baadi
    >yasser bjarho

--- academie.java
+++ academie.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+import java.util.Vector;
+
 public class academie {
+    public String name;
+    private Vector<college> colleges;
     
+    public academie(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("donne le nome de academeie :");
+        name = sc.next();
+        this.colleges = new Vector<college>();
+    }
+    public void ajout_college(college col){
+        colleges.add(col);
+    }
 }
--- college.java
+++ college.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+import java.util.Vector;
+
 public class college {
-    
+    private String name ;
+    private String site_web;
+    private Vector<departement> depa;
+
+    public college(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("donne le nom de college :");
+        name = sc.next();
+        System.out.println("donne le site web de college :");
+        site_web = sc.next();
+        this.depa = new Vector<departement>();
+    }
+    public void ajout_departement(departement depar){
+        depa.add(depar);
+    }
 }
